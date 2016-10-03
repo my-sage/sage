4b1c8fb75cd6b667682f89508093608f1afc6334
Bruno Raposo <braposo@g.harvard.edu>
2016-10-03 12:05:30 -0400

    prepare to merge

---
+++ server/main.js
@@ -0,0 +1,29 @@
+'use strict';
+var chalk = require('chalk');
+var db = require('./db');
+
+// Create a node server instance! cOoL!
+var server = require('http').createServer();
+
+var createApplication = function () {
+    var app = require('./app')(db);
+    server.on('request', app); // Attach the Express application.
+    // require('./io')(server);   // Attach socket.io.
+};
+
+var startServer = function () {
+
+    var PORT = process.env.PORT || 1337;
+
+    server.listen(PORT, function () {
+        console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
+    });
+
+};
+
+db.sync()
+.then(createApplication)
+.then(startServer)
+.catch(function (err) {
+    console.error(chalk.red(err.stack));
+});
