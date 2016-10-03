1ff9e4015c49f12edd86af5c680e58ff2c339fc1
Aditya K <adityavkk@gmail.com>
2016-10-03 17:21:45 -0400

    add getCurrentBudgets class method to budgets model

--- server/db/models/budget.js
+++ server/db/models/budget.js
@@ -36,10 +36,24 @@ fields.endDate = {
 let hooks = {}
 options.hooks = hooks;
 
-hooks.beforeCreate = function (budget) {
+
+hooks.beforeCreate = function(budget) {
   let currentTime = new Date()
-  let endOfMonth = new Date(currentTime.getFullYear(), currentTime.getMonth()+1, 0)
+  let endOfMonth = new Date(currentTime.getFullYear(), currentTime.getMonth() + 1, 0)
   budget.endDate = endOfMonth.valueOf()
 }
+
+let classMethods = {};
+classMethods.getCurrentBudgets = function() {
+  let currentUnixTime = new Date().valueOf();
+  return this.findAll({
+    where: {
+      endDate: {
+        $gt: currentUnixTime
+      }
+    }
+  })
+}
+options.classMethods = classMethods;
 
 module.exports = db.define('budget', fields, options)
