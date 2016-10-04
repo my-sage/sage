0a45be914bc24b50ad71d60322d3d4ebec65a993
Aditya K <adityavkk@gmail.com>
2016-10-03 20:04:11 -0400

    change test spec to create Education category

--- tests/db/models/budget.test.js
+++ tests/db/models/budget.test.js
@@ -55,7 +55,7 @@ describe('Budget Model', function() {
       })
       .then(() => {
         let creatingCategories = [{
-          name: 'CHeese'
+          name: 'Education'
         }, {
           name: 'Clothing'
         }]
