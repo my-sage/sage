eed84593234ec7daeca9054905dd69437f97d1a6
Christopher Miller <millercs624@gmail.com>
2016-10-06 21:03:02 -0400

    fix typo

--- server/db/models/transaction.js
+++ server/db/models/transaction.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Sequelize = require('Sequelize');
+const Sequelize = require('sequelize');
 const db = require('../_db');
 const Account = require('./account');
 const Merchant = require('./merchant');
