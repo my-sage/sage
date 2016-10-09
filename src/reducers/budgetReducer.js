2cba4f709e4a03e618cd0bca9faf15094f91196f
Christopher Miller <millercs624@gmail.com>
2016-10-09 14:12:16 -0400

    prepare to merge

--- src/reducers/budgetReducer.js
+++ src/reducers/budgetReducer.js
@@ -1,20 +1,43 @@
 'use strict';
 
+import { combineReducers } from 'redux';
 import * as actions from '../actions/constants/budgetActionTypes';
 import { createReducer } from '../utils';
 import { evolve, map, filter } from 'ramda';
+import initialState from './initialState';
 
-const handlers = {
+const { budgets } = initialState;
+
+const dataHandlers = {
   [actions.CREATE_BUDGET](state, action) {
     return state;
   },
   [actions.DELETE_BUDGET](state, action) {
     return state.filter(budget => budget);
   },
-  [actions.LOAD_BUDGETS_SUCCESS](state, action) {
+  [actions.FETCH_BUDGETS_SUCCESS](state, action) {
     console.log(action.budgets);
     return action.budgets;
   }
 };
 
-export default createReducer([], handlers);
+const data = createReducer(budgets.data, dataHandlers);
+
+const isFetchingHandlers = {
+  [actions.FETCH_BUDGETS_REQUEST](state, action) {
+    return true;
+  },
+  [actions.FETCH_BUDGETS_SUCCESS](state, action) {
+    return false;
+  },
+  [actions.FETCH_BUDGETS_FAIL](state, action) {
+    return false;
+  }
+}
+
+const isFetching= createReducer(budgets.isFetching, isFetchingHandlers)
+
+export default combineReducers({
+  data,
+  isFetching
+});
