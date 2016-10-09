fec31f251db577ccce81aa91728452313c2312ec
Christopher Miller <millercs624@gmail.com>
2016-10-09 14:42:10 -0400

    new reducer pattern

--- src/actions/transactionActions.js
+++ src/actions/transactionActions.js
@@ -1,22 +1,23 @@
 'use strict';
 
 import * as transactionApi from '../api/transaction';
-import {CREATE_TRANSACTION, UPDATE_TRANSACTION, DELETE_TRANSACTION, GET_TRANSACTION, LOAD_TRANSACTIONS_SUCCESS, LOADING_TRANSACTIONS, LOAD_TRANSACTIONS_FAIL} from './constants/transactionActionTypes';
+import {CREATE_TRANSACTION, UPDATE_TRANSACTION, DELETE_TRANSACTION, GET_TRANSACTION, FETCH_TRANSACTIONS_SUCCESS, FETCH_TRANSACTIONS_REQUEST, FETCH_TRANSACTIONS_FAIL} from './constants/transactionActionTypes';
 import { makeActionCreator, dispatchFail } from '../utils/';
 
 export const createTransaction = makeActionCreator(CREATE_TRANSACTION, 'transaction');
 export const updateTransaction = makeActionCreator(UPDATE_TRANSACTION, 'id', 'transaction');
-export const getTransaction = makeActionCreator(GET_TRANSACTION, 'id');
 export const deleteTransaction = makeActionCreator(DELETE_TRANSACTION, 'id');
-export const loadTransactionsSuccess = makeActionCreator(LOAD_TRANSACTIONS_SUCCESS, 'transactions');
-export const loadTransactionsFail = makeActionCreator(LOAD_TRANSACTIONS_FAIL, 'error');
+export const fetchTransactions = makeActionCreator(FETCH_TRANSACTIONS_REQUEST);
+export const fetchTransactionsSuccess = makeActionCreator(FETCH_TRANSACTIONS_SUCCESS, 'transactions');
+export const fetchTransactionsFail = makeActionCreator(FETCH_TRANSACTIONS_FAIL, 'error');
 
 export function loadTransactions() {
 	return function(dispatch) {
+		dispatch(fetchTransactions());
 		return transactionApi.getAllTransactions()
 			.then(transactions => {
-				dispatch(loadTransactionsSuccess(transactions));
+				dispatch(fetchTransactionsSuccess(transactions));
 			})
-			.catch(dispatchFail(dispatch, loadTransactionsFail))
+			.catch(dispatchFail(dispatch, fetchTransactionsFail));
 	}
 }
--- src/reducers/transactionReducer.js
+++ src/reducers/transactionReducer.js
@@ -1,31 +1,36 @@
 'use strict';
 
-import { CREATE_TRANSACTION, UPDATE_TRANSACTION, DELETE_TRANSACTION, GET_TRANSACTION, LOAD_TRANSACTIONS_SUCCESS, LOADING_TRANSACTIONS, LOAD_TRANSACTIONS_FAIL} from '../actions/constants/transactionActionTypes';
+import { CREATE_TRANSACTION, DELETE_TRANSACTION, FETCH_TRANSACTIONS_SUCCESS, FETCH_TRANSACTIONS_REQUEST, FETCH_TRANSACTIONS_FAIL} from '../actions/constants/transactionActionTypes';
 import {createReducer} from '../utils';
 import {evolve, map, filter} from 'ramda';
+import initialState from './initialState';
 
-const handlers = {
+const {transactions} = initialState;
+
+const dataHandlers = {
 	[CREATE_TRANSACTION](state, action) {
-		console.log('WORK IN PROGRESS');
-		return state;
-	},
-	[UPDATE_TRANSACTION](state, action) {
-		console.log('WORK IN PROGRESS');
 		return state;
 	},
 	[DELETE_TRANSACTION](state, action){
 		return state.filter(transaction => transaction);
 	},
-	[GET_TRANSACTION](state, action){
-		return [...state, action.transaction]
-	},
-	[LOAD_TRANSACTIONS_SUCCESS](state, action){
-		return {data: action.transactions, isFetching: false};
-	},
-	[LOAD_TRANSACTIONS_FAIL](state, action){
-		console.log('WORK IN PROGRESS');
-		return state;
+	[FETCH_TRANSACTIONS_SUCCESS](state, action){
+		return action.transactions;
 	}
 };
 
-export default createReducer([], handlers);
+const isFetchingHandlers = {
+	[FETCH_TRANSACTIONS_REQUEST](state, action) {
+		return true;
+	},
+	[FETCH_TRANSACTIONS_SUCCESS](state, action) {
+		return false;
+	},
+	[FETCH_TRANSACTIONS_FAIL](state, action) {
+		return false;
+	}
+};
+
+const isFetching = createReducer(transactions.isFetching, isFetchingHandlers);
+
+export default combineReducers({data, isFetching});
