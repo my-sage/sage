b07ae6d451518fe51fcea3617cde9b5e0b3593cf
Christopher Miller <millercs624@gmail.com>
2016-10-09 13:15:14 -0400

    complete reducer skeleton

--- src/reducers/transactionReducer.js
+++ src/reducers/transactionReducer.js
@@ -1,28 +1,31 @@
 'use strict';
 
-import {CREATE_TRANSACTION, UPDATE_TRANSACTION, DELETE_TRANSACTION, GET_TRANSACTION, LOAD_TRANSACTIONS_SUCCESS, LOADING_TRANSACTIONS, LOAD_TRANSACTIONS_FAIL} from '../actions/constants/transactionActionTypes';
-import { createReducer } from '../utils';
-import { evolve, map, filter } from 'ramda';
+import { CREATE_TRANSACTION, UPDATE_TRANSACTION, DELETE_TRANSACTION, GET_TRANSACTION, LOAD_TRANSACTIONS_SUCCESS, LOADING_TRANSACTIONS, LOAD_TRANSACTIONS_FAIL} from '../actions/constants/transactionActionTypes';
+import {createReducer} from '../utils';
+import {evolve, map, filter} from 'ramda';
 
 const handlers = {
 	[CREATE_TRANSACTION](state, action) {
+		console.log('WORK IN PROGRESS');
 		return state;
 	},
 	[UPDATE_TRANSACTION](state, action) {
-
+		console.log('WORK IN PROGRESS');
+		return state;
 	},
 	[DELETE_TRANSACTION](state, action){
 		return state.filter(transaction => transaction);
 	},
 	[GET_TRANSACTION](state, action){
-
+		return [...state, action.transaction]
 	},
 	[LOAD_TRANSACTIONS_SUCCESS](state, action){
-		return action.transactions;
+		return {data: action.transactions, isFetching: false};
 	},
 	[LOAD_TRANSACTIONS_FAIL](state, action){
-
-	},
+		console.log('WORK IN PROGRESS');
+		return state;
+	}
 };
 
 export default createReducer([], handlers);
