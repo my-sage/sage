9d9ee83bcb1790162584c5ed9de5e990b3c1a57c
Bruno Raposo <braposo@g.harvard.edu>
2016-10-10 16:35:08 -0400

    more html

--- src/components/topNavbar/TopNavbar.js
+++ src/components/topNavbar/TopNavbar.js
@@ -32,3 +32,26 @@ export default React.createClass ({
 		)
 	}
 })
+
+
+
+// <div className="navbar navbar-default navbar-fixed-top inverse" role="navigation">
+//       <div className="container">
+//         <div className="navbar-header">
+//           <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+//             <span className="sr-only">Toggle navigation</span>
+//             <span className="icon-bar"></span>
+//             <span className="icon-bar"></span>
+//             <span className="icon-bar"></span>
+//           </button>
+//           <a className="navbar-brand" href="#">Project name</a>
+//         </div>
+//         <div className="navbar-collapse collapse">
+//           <ul className="nav navbar-nav">
+//             <li className="active"><a href="#">Home</a></li>
+//             <li><a href="#about">About</a></li>
+//             <li><a href="#contact">Contact</a></li>
+//           </ul>
+//         </div>
+//       </div>
+//   </div>
