80ee2347055a2c631f7a2c7e996bf0b8b0b4cf3e
Bruno Raposo <braposo@g.harvard.edu>
2016-10-11 19:29:38 -0400

    cool hover

--- src/components/leftNav/LeftNav.js
+++ src/components/leftNav/LeftNav.js
@@ -4,8 +4,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Link, IndexLink } from 'react-router'
 import Logo from '../../images/logo.png'
+import Radium from 'radium'
 
-const divStyle = {
+const styles = {
+  divStyle: {
     height: '100%',
     width: '250px',
     position: 'fixed',
@@ -16,35 +18,25 @@ const divStyle = {
     overflowX: 'hidden',
     transition: '0.5s',
     paddingTop: '20px'
-}
-
-const button = {
-  width: '100%',
-  padding: '35px 0px',
-  background: "transparent",
-  border: '1px solid white',
-  borderLeft: 'transparent',
-  borderRight: 'transparent',
-  outline: 0
-}
-
-const logo = {
-  marginLeft: "10px", 
-  marginBottom:"20px", 
-  maxWidth: "230px"
+  },
+  logo: {
+    marginLeft: "10px", 
+    marginBottom:"40px", 
+    maxWidth: "230px"
+  }
 }
 
 export default React.createClass ({
      render(){
           return (
             <div>
-              <div style={divStyle}>
-              <img src={Logo} style={logo}/>
-                 <Link to="/" activeClassName="selected" onlyActiveOnIndex={true}><button style={button}><span className="glyphicon glyphicon-th"></span>  Overview</button></Link>
-                 <Link to="/transactions" activeClassName="selected"><button style={button}><span className="glyphicon glyphicon-th-list"></span>  Transactions</button></Link>
-                 <Link to="/budgets" activeClassName="selected"><button style={button}><span className="glyphicon glyphicon-exclamation-sign"></span>  Budgets</button></Link>
-                 <Link to="/trends" activeClassName="selected"><button style={button}><span className="glyphicon glyphicon-stats"></span>  Trends</button></Link>
-                 <Link to="/settings" activeClassName="selected"><button style={button}><span className="glyphicon glyphicon-leaf"></span>  Settings</button></Link>
+              <div style={styles.divStyle}>
+              <img src={Logo} style={styles.logo}/>
+                 <Link to="/" activeClassName="selected" onlyActiveOnIndex={true}><button className="buttoncss"><span className="glyphicon glyphicon-th"></span>  Overview</button></Link>
+                 <Link to="/transactions" activeClassName="selected"><button className="buttoncss"><span className="glyphicon glyphicon-th-list"></span>  Transactions</button></Link>
+                 <Link to="/budgets" activeClassName="selected"><button className="buttoncss"><span className="glyphicon glyphicon-exclamation-sign"></span>  Budgets</button></Link>
+                 <Link to="/trends" activeClassName="selected"><button className="buttoncss"><span className="glyphicon glyphicon-stats"></span>  Trends</button></Link>
+                 <Link to="/settings" activeClassName="selected"><button className="buttoncss"><span className="glyphicon glyphicon-leaf"></span>  Settings</button></Link>
                </div>
             </div>
         )
