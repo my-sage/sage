df24cba1d9f401100f40c038a06fd84b209ad51a
Aditya K <adityavkk@gmail.com>
2016-10-11 18:25:25 -0400

    banking api connec

---
+++ server/banking/credentials.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const amex = {
+  user: 'amex16hertz',
+  password: 'Moringa1',
+  accId: '372720382612029',
+}
+
+const boa = {
+  user: 'jmspinosa',
+  password: '',
+}
+
+module.exports = {
+  amex, boa
+}
