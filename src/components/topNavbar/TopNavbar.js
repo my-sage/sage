eb555353cf1a857161722f457f1c27cdc1d6bff4
Bruno Raposo <braposo@g.harvard.edu>
2016-10-12 14:56:30 -0400

    moar stuff

--- src/components/topNavbar/TopNavbar.js
+++ src/components/topNavbar/TopNavbar.js
@@ -5,7 +5,7 @@ import Bootstrap, {Navbar, NavItem, Nav, NavDropdown, MenuItem} from 'react-boot
 import { render } from 'react-dom';
 
 const divStyle = {
-	marginLeft: "250px"
+	marginLeft: "249px"
 }
 
 const float = {
@@ -24,34 +24,3 @@ export default React.createClass ({
 		)
 	}
 })
-
-
-// <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-//   <MenuItem eventKey={3.1}>Action</MenuItem>
-//   <MenuItem eventKey={3.2}>Another action</MenuItem>
-//   <MenuItem eventKey={3.3}>Something else here</MenuItem>
-//   <MenuItem divider />
-//   <MenuItem eventKey={3.3}>Separated link</MenuItem>
-// </NavDropdown>
-
-
-// <div className="navbar navbar-default navbar-fixed-top inverse" role="navigation">
-//       <div className="container">
-//         <div className="navbar-header">
-//           <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-//             <span className="sr-only">Toggle navigation</span>
-//             <span className="icon-bar"></span>
-//             <span className="icon-bar"></span>
-//             <span className="icon-bar"></span>
-//           </button>
-//           <a className="navbar-brand" href="#">Project name</a>
-//         </div>
-//         <div className="navbar-collapse collapse">
-//           <ul className="nav navbar-nav">
-//             <li className="active"><a href="#">Home</a></li>
-//             <li><a href="#about">About</a></li>
-//             <li><a href="#contact">Contact</a></li>
-//           </ul>
-//         </div>
-//       </div>
-//   </div>
