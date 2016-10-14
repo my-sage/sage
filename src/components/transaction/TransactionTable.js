c9f1e77a8959381510f57abf52fd4e3f3e0b0818
Tony <yechao@umich.edu>
2016-10-14 16:22:31 -0400

    sorting date and amount of transaction

--- src/components/transaction/TransactionTable.js
+++ src/components/transaction/TransactionTable.js
@@ -1,27 +1,61 @@
 'use strict';
 
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import TransactionSingle from './TransactionSingle';
 import { Table } from "react-bootstrap";
+import R from 'ramda';
 
-const TransactionTable = ({transactions}) => {
-	return (
-		<Table striped bordered condensed hover responsive>
-			<thead>
-				<tr style={headerBar}>
-					<th style={header}>Date</th>
-					<th style={header}>Merchant</th>
-					<th style={header}>Category</th>
-					<th style={header}>Amount</th>
-					<th style={header}>Edit</th>
-				</tr>
-			</thead>
-			<tbody>
-			{transactions.map(transaction=>TransactionSingle(transaction))}
-			</tbody>
-		</Table>
-	)
-};
+class TransactionTable extends Component {
+
+	constructor (props) {
+		super(props);
+		this.state = {
+			transactions: this.props.transactions,
+			status: {date: false, amount: false}
+		}
+		this.Sort = this.Sort.bind(this);
+		console.log('gettting the state', this.state.transactions)
+	}
+
+	Sort (event) {
+		//console.log('changing property',event.target.attr('name'));
+		let field = event.target.getAttribute('name')
+		if(this.state.status[field]===false){
+			console.log('field', event.target.getAttribute('name'));
+			let transactions = R.sortBy(R.prop(field), this.state.transactions);
+			console.log('sorted transactions',transactions);
+			
+			for(let props in this.state.status) {
+				this.state.status[props] = false;
+			}
+			this.state.status[field] = true;
+
+			return this.setState({transactions: transactions})
+		}else{
+			let transactions = this.state.transactions.reverse();
+			return this.setState({transactions: transactions})
+		}
+	}
+
+	render () {
+		return (
+			<Table striped bordered condensed hover responsive>
+				<thead>
+					<tr style={headerBar}>
+						<th style={header} name="date" onClick={this.Sort}>Date</th>
+						<th style={header} name="merchantId" onClick={this.Sort}>Merchant</th>
+						<th style={header} name="categoryId" onClick={this.Sort}>Category</th>
+						<th style={header} name="amount" onClick={this.Sort}>Amount</th>
+						<th style={header}>Edit</th>
+					</tr>
+				</thead>
+				<tbody>
+				{this.state.transactions.map(transaction=>TransactionSingle(transaction))}
+				</tbody>
+			</Table>
+		)
+	}
+}
 
 const tableStyle = {
       broder: "1px solid black"
