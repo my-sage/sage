257babca3fa778d360802c3fc22f2705f5923f5f
Bruno Raposo <braposo@g.harvard.edu>
2016-10-15 12:56:44 -0400

    colors

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     return (
       <div>
           <TopNavbar/>
-          <BottomNavbar/>
+          
           <div>
             <LeftNav/>
               <div className="main">
