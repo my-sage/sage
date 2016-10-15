99f3430093b1b2074d36a2a42b4356d3401c0e31
Bruno Raposo <braposo@g.harvard.edu>
2016-10-15 16:30:11 -0400

    ok its fixed

--- src/components/topNavbar/HamburgerMenu.js
+++ src/components/topNavbar/HamburgerMenu.js
@@ -9,11 +9,11 @@ export default React.createClass({
   render() {
     return (
       <Menu className="burgerMenu">
-        <RadiumLink to="/" activeClassName="selected" onlyActiveOnIndex={true}><button className="buttoncssburger"><i className="fa fa-tachometer" aria-hidden="true"></i> &nbsp; Overview</button></RadiumLink>
-        <RadiumLink to="/transactions" activeClassName="selected"><button className="buttoncssburger"><i className="fa fa-money" aria-hidden="true"></i> &nbsp; Transactions</button></RadiumLink>
-        <RadiumLink to="/budgets" activeClassName="selected"><button className="buttoncssburger"><i className="fa fa-tasks" aria-hidden="true"></i>  &nbsp; Budgets</button></RadiumLink>
-        <RadiumLink to="/trends" activeClassName="selected"><button className="buttoncssburger"><i className="fa fa-line-chart" aria-hidden="true"></i>  &nbsp; Trends</button></RadiumLink>
-        <RadiumLink to="/settings" activeClassName="selected"><button className="buttoncssburger"><span className="glyphicon glyphicon-leaf"></span>  &nbsp; Settings</button></RadiumLink>
+        <RadiumLink to="/overview" activeClassName="selected" onlyActiveOnIndex={true}><button className="buttoncss"><i className="fa fa-tachometer" aria-hidden="true"></i> &nbsp; Overview</button></RadiumLink>
+        <RadiumLink to="/transactions" activeClassName="selected"><button className="buttoncss"><i className="fa fa-money" aria-hidden="true"></i> &nbsp; Transactions</button></RadiumLink>
+        <RadiumLink to="/budgets" activeClassName="selected"><button className="buttoncss"><i className="fa fa-tasks" aria-hidden="true"></i>  &nbsp; Budgets</button></RadiumLink>
+        <RadiumLink to="/trends" activeClassName="selected"><button className="buttoncss"><i className="fa fa-line-chart" aria-hidden="true"></i>  &nbsp; Trends</button></RadiumLink>
+        <RadiumLink to="/settings" activeClassName="selected"><button className="buttoncss"><span className="glyphicon glyphicon-leaf"></span>  &nbsp; Settings</button></RadiumLink>
       </Menu>
     );
   }
