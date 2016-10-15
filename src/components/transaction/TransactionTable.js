5bfb032c39f1591391460e0616ce8a0989f1a219
Tony <yechao@umich.edu>
2016-10-14 21:15:16 -0400

    finish date and amount sorting, and the arrow indicating the sort

--- src/components/transaction/TransactionTable.js
+++ src/components/transaction/TransactionTable.js
@@ -11,28 +11,42 @@ class TransactionTable extends Component {
 		super(props);
 		this.state = {
 			transactions: this.props.transactions,
-			status: {date: false, amount: false}
+			status: {date: '', amount: ''},
+			arrow: true
 		}
 		this.Sort = this.Sort.bind(this);
-		console.log('gettting the state', this.state.transactions)
+		this.arrow = this.arrow.bind(this);
+	}
+
+	arrow (field) {
+
+			if(this.state.arrow) {
+				this.state.status[field] = <i className="fa fa-sort-asc" aria-hidden="true"></i>;
+			}else{
+				this.state.status[field] = <i className="fa fa-sort-desc" aria-hidden="true"></i>
+			}
 	}
 
 	Sort (event) {
-		//console.log('changing property',event.target.attr('name'));
-		let field = event.target.getAttribute('name')
-		if(this.state.status[field]===false){
-			console.log('field', event.target.getAttribute('name'));
+		let field = event.target.getAttribute('name');
+
+		if(this.state.status[field]===''){
+
 			let transactions = R.sortBy(R.prop(field), this.state.transactions);
-			console.log('sorted transactions',transactions);
 			
 			for(let props in this.state.status) {
-				this.state.status[props] = false;
+				this.state.status[props] = '';
 			}
-			this.state.status[field] = true;
+
+			this.state.arrow = true;
+			this.arrow(field);
+			console.log('the arrow element', this.arrow)
 
 			return this.setState({transactions: transactions})
 		}else{
 			let transactions = this.state.transactions.reverse();
+			this.state.arrow = !this.state.arrow;
+			this.arrow(field);
 			return this.setState({transactions: transactions})
 		}
 	}
@@ -42,10 +56,10 @@ class TransactionTable extends Component {
 			<Table striped bordered condensed hover responsive>
 				<thead>
 					<tr style={headerBar}>
-						<th style={header} name="date" onClick={this.Sort}>Date</th>
+						<th style={header} name="date" onClick={this.Sort}>Date {this.state.status.date}</th>
 						<th style={header} name="merchantId" onClick={this.Sort}>Merchant</th>
 						<th style={header} name="categoryId" onClick={this.Sort}>Category</th>
-						<th style={header} name="amount" onClick={this.Sort}>Amount</th>
+						<th style={header} name="amount" onClick={this.Sort}>Amount {this.state.status.amount}</th>
 						<th style={header}>Edit</th>
 					</tr>
 				</thead>
