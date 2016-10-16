d561335a9a003a82126cbe75cd75e99d643f1d51
Bruno Raposo <braposo@g.harvard.edu>
2016-10-16 19:19:32 -0400

    trends looking awesome

--- src/components/trends/IncomePage.js
+++ src/components/trends/IncomePage.js
@@ -20,7 +20,9 @@ class IncomePage extends Component {
 		const shouldBePie = (boolean) => boolean ? <PieChart data={enhancedTransactions} groupBy={this.state.groupBy}/> : <VerticalBarGraph data={enhancedTransactions} groupBy={this.state.groupBy} barColor='#2ecc71'/>
 		return (
 			<div>
-				<FilterContainer />
+				<div style={{paddingTop:'20px'}}>
+					<FilterContainer />
+				</div>
 				<GroupingDropdown onSelect={groupSetter.bind(this)}/>
 				<ChartSelect onSelect={chartSelector.bind(this)}/>
 				<h3>Income By {this.state.displayName}</h3>
